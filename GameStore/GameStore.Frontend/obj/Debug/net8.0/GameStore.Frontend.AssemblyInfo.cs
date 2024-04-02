c592120d1f69606f19b4d4fb952f18724cf79880
Will Sneddon <will@sneddon.dev>
2024-04-02 17:17:12 -0400

    Add gitignore, add updated assembly infos

--- GameStore/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore/GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37c1fa31068036ac559d7adcb66ef13e2cdfd19")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+075a49681fe7fb74ccceb9c09e18b792cb6db848")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
