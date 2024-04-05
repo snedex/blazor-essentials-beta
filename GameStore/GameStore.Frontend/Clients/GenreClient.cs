9192c03b933a4317f9fcc2803e086af4da7daa79
Will Sneddon <will@sneddon.dev>
2024-04-05 19:16:07 -0400

    Add placeholder Genres client and model

---
+++ GameStore/GameStore.Frontend/Clients/GenreClient.cs
@@ -0,0 +1,16 @@
+﻿using GameStore.Frontend.Models;
+
+namespace GameStore.Frontend.Clients;
+
+public class GenreClient
+{
+    private readonly Genre[] genres = [
+        new() { Id = 1, Name = "Fighting" },
+        new() { Id = 2, Name = "Roleplaying" },
+        new() { Id = 3, Name = "Sports" },
+        new() { Id = 4, Name = "Racing" },
+        new() { Id = 5, Name = "Kids and Family" },
+    ];
+
+    public Genre[] GetGenres() => genres;
+}
