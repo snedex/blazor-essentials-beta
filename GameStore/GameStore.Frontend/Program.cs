98a133e619c94024f9cde141fd39c3db8d70907b
Will Sneddon <will@sneddon.dev>
2024-04-05 21:21:21 -0400

    Add interactive server rendering for delete

--- GameStore/GameStore.Frontend/Program.cs
+++ GameStore/GameStore.Frontend/Program.cs
@@ -4,7 +4,8 @@ using GameStore.Frontend.Components;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents();
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
 builder.Services.AddSingleton<GamesClient>();
 builder.Services.AddSingleton<GenreClient>();
 
@@ -23,6 +24,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
-app.MapRazorComponents<App>();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
 
 app.Run();
